292813568e1435367e3a482d1d9bee654deee626
RyanBLeon28 <ryanbleon0@gmail.com>
2024-03-11 23:24:12 -0300

    anemometro

--- Anemometro/Codigos Sensor Hall/sensor_mag_interrup/sensor_mag_interrup.ino
+++ Anemometro/Codigos Sensor Hall/sensor_mag_interrup/sensor_mag_interrup.ino
@@ -16,48 +16,44 @@
 
 // ============================================================================
 // --- Mapeamento de Hardware ---
-#define    hall    (1<<PD2)
+//#define    hall    (1<<PD2)
 
 // ============================================================================
 // --- Variáveis Globais ---
-unsigned counter = 0x00;                       // contador de pulsos
-uint16_t  ptime = 0;                           // tempo entre uma volta completa
+unsigned counter       = 0x00;                 //contador de pulsos
+int pulsosPorRevolucao = 3,                    //total de imãs
+    rpm                   ,
+    freqAngular           ;
 
+uint16_t  periodo;                             //tempo entre uma volta completa
+uint16_t  overFlowTime;                        //tempo para verificar os pulsos
 // ============================================================================
 // --- Interrupções ---
 ISR(INT0_vect)
 {
-  if(!(TCCR1B&(1<<CS11)))                      //timer1 desligado?
-  {                                            //sim
+  counter ++;
+
+  if(counter == 1)                             //começa a contar o tempo para uma volta
+  {                                           
     TCCR1B |= (1<<CS11);                       //habilita contagem com PS 1:8
-	  //EICRA &= ~(1<<ISC00);                      //configura interrupção externa para borda de descida
   }
-  
+  if(counter == 2 || counter == 3)
+  {
+    return;
+  }
   else
   {
     TCCR1B = 0x00;                             //desliga timer1
-    ptime  = TCNT1;                            //salva largura de pulso em µs x 2
+    periodo  = TCNT1;                          //salva tempo de uma volta em µs x 2
+    periodo >>= 1;                             //divide por 2 o valor anterior
     TCNT1  = 0x00;                             //reinicia valor do timer1
-    EICRA |= (1<<ISC00);                       //configura interrupção externa para borda de subida  
-    ptime >>= 1;
   }
-	
 } //end ISR INT0
 
-ISR(TIMER0_OVF_vect)                         //Rotina de interrupção
+ISR(TIMER0_OVF_vect)                           //Rotina de interrupção
 {
-	
-	//base de tempo de 1ms
-	TCNT0 = 0x06;                              //reinicializa Timer0
-	counter++;
-	
-	if(cnt==80)
+	if(cnt==49)                                  //Total de 200ms            
 	{
-	  //base de tempo de 80ms 
-	  cnt = 0;
-	  PORTC |=  hall;
-	  _delay_us(10);
-	  PORTC &= ~hall;	
 	 
 	} //end if cnt
 
@@ -65,36 +61,42 @@ ISR(TIMER0_OVF_vect)                         //Rotina de interrupção
 
 void setup() {  
   //Configuração das Interrupções.
-  
-  cli();                                       //desliga interrupções
+  cli();                                       //Desabilita interrupções
 
   DDRD &= ~(1<<DDD2);                          //configura PD2 como entrada  *registrador fica setado como zero
 
   PORTD |= (1<<PORTD2);                        //habilita pull-up interno de PD
   
   TCNT0  = 0x00;                               //inicia Timer0 
-  TCCR0B = 0x04;                               //configura o prescaler para 1:256
+  TCCR0B = 0x04;                               //configura o prescaler para 1:256 (guardara o valor de overflow)
+  TCCR1B = 0x00;                               //desliga timer1                   (guardara o valor de uma volta)
   TIMSK0 = 0x01;                               //habilita interrupção por overflow do Timer0
   EICRA |= (1<<ISC01) | (1<<ISC00);            //interrupção externa por borda de subida inicialmente
   EIMSK |= (1<<INT0);                          //habilita interrupção externa
   
-  sei();                                       //habilita as interrupções
+  sei();                                       //Habilita as interrupções
   
   Wire.begin();
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
-  sens_RPM();
+  sens_RPM(periodo);
 }
 
 
 // ============================================================================
 // Calcula RPM conforme sensor
-void sens_RPM(uint16_t pulse, int gval, int yval, int rval)
+void sens_RPM(uint16_t pulse)
 {
-
+  pulse = periodo/1000000                      //converte de µs para s
    
- 	
+ 	freqAngular = (2*3.14) / pulse;
+  Serial.print("Frequencia angular: ");
+  Serial.println(freqAngular);
+
+  rpm = 60 / pulse;
+  Serial.print("RPM: ");
+  Serial.println(rpm);
 } 
 
